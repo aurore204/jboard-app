34f9eb3251cbf2f3fba7e82b442c2b13298d838a
aurore204 <aurorenjimegne@gmail.com>
2025-10-07 11:15:15 +0200

    ma base modifie

---
+++ offre_emploi1.sql
@@ -0,0 +1,190 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1deb3
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:3306
+-- Généré le : mar. 07 oct. 2025 à 09:08
+-- Version du serveur : 8.0.43-0ubuntu0.24.04.2
+-- Version de PHP : 8.4.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `offre_emploi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `annonces`
+--
+
+CREATE TABLE `annonces` (
+  `id_annonces` int NOT NULL,
+  `titre` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `id_comp` int NOT NULL,
+  `lieu` varchar(255) DEFAULT NULL,
+  `type_contrat` enum('CDI','CDD','Stage','Freelance') DEFAULT 'CDI',
+  `salaire` varchar(50) DEFAULT NULL,
+  `date_publication` datetime DEFAULT CURRENT_TIMESTAMP,
+  `date_limite` date DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `annonces`
+--
+
+INSERT INTO `annonces` (`id_annonces`, `titre`, `description`, `id_comp`, `lieu`, `type_contrat`, `salaire`, `date_publication`, `date_limite`, `created_at`, `updated_at`) VALUES
+(1, 'RECHERCHE DEVELOPPEUR', 'un developpeur qui sait faire\nau django et du react', 1, 'paris-montparnasse', 'CDI', '2000euros', '2025-10-07 11:05:31', '2025-10-30', '2025-10-07 11:05:31', '2025-10-07 11:05:31');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `candidatures`
+--
+
+CREATE TABLE `candidatures` (
+  `id_candidature` int NOT NULL,
+  `id_annonces` int NOT NULL,
+  `id_pers` int NOT NULL,
+  `status` enum('pending','accepted','rejected') DEFAULT 'pending',
+  `email_sent` tinyint(1) DEFAULT '0',
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `companies`
+--
+
+CREATE TABLE `companies` (
+  `id_comp` int NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `password` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `companies`
+--
+
+INSERT INTO `companies` (`id_comp`, `name`, `email`, `phone`, `address`, `created_at`, `updated_at`, `password`) VALUES
+(1, 'indeed', 'indeed@indeed.com', '+33 55 88 77', 'issy-les-molineaux', '2025-10-07 10:48:06', '2025-10-07 10:48:06', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `people`
+--
+
+CREATE TABLE `people` (
+  `id_pers` int NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `cv` varchar(255) DEFAULT NULL,
+  `role` enum('responsable','postulant') NOT NULL,
+  `horaire_travail` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `annonces`
+--
+ALTER TABLE `annonces`
+  ADD PRIMARY KEY (`id_annonces`),
+  ADD KEY `id_entreprise` (`id_comp`);
+
+--
+-- Index pour la table `candidatures`
+--
+ALTER TABLE `candidatures`
+  ADD PRIMARY KEY (`id_candidature`),
+  ADD KEY `id_annonces` (`id_annonces`),
+  ADD KEY `id_pers` (`id_pers`);
+
+--
+-- Index pour la table `companies`
+--
+ALTER TABLE `companies`
+  ADD PRIMARY KEY (`id_comp`);
+
+--
+-- Index pour la table `people`
+--
+ALTER TABLE `people`
+  ADD PRIMARY KEY (`id_pers`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `annonces`
+--
+ALTER TABLE `annonces`
+  MODIFY `id_annonces` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `candidatures`
+--
+ALTER TABLE `candidatures`
+  MODIFY `id_candidature` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `companies`
+--
+ALTER TABLE `companies`
+  MODIFY `id_comp` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `people`
+--
+ALTER TABLE `people`
+  MODIFY `id_pers` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `annonces`
+--
+ALTER TABLE `annonces`
+  ADD CONSTRAINT `annonces_ibfk_1` FOREIGN KEY (`id_comp`) REFERENCES `companies` (`id_comp`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `candidatures`
+--
+ALTER TABLE `candidatures`
+  ADD CONSTRAINT `candidatures_ibfk_1` FOREIGN KEY (`id_annonces`) REFERENCES `annonces` (`id_annonces`) ON DELETE CASCADE,
+  ADD CONSTRAINT `candidatures_ibfk_2` FOREIGN KEY (`id_pers`) REFERENCES `people` (`id_pers`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
