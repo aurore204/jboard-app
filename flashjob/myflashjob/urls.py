85f5eead17daa4258ea977250a2ea043d4d9eec9
Amaury <amaury.alriq@epitech.eu>
2025-10-16 09:39:38 +0200

    mise a jour

--- flashjob/myflashjob/urls.py
+++ flashjob/myflashjob/urls.py
@@ -4,7 +4,12 @@ from . import views
 urlpatterns = [
     path('api/personnes/', views.personnes_list, name='personnes_list'),
     path('api/entreprises/', views.entreprises_list, name='entreprises_list'),
+    path('api/entreprises/create/', views.create_entreprise, name='create_entreprise'),
+    path('api/personnes/create/', views.create_personnes, name='create_personnes'),
     path('api/annonces/', views.annonces_list, name='annonces_list'),
     path('api/candidatures/', views.candidatures_list, name='candidatures_list'),
-    path('api/annonces/<int:id>', views.annonces_details_list, name='annonces_list'),
+    path('api/annonces/<int:id>/', views.annonces_details_list, name='annonces_details'),
+    path('api/personnes/<int:id>/', views.personnes_details_list, name='personnes_details_list'),
+    path('api/entreprises/<int:id>/', views.entreprises_details_list, name='entreprises_details_list'),
+    path('api/login/', views.login, name='login'),
 ]
--- flashjob/myflashjob/views.py
+++ flashjob/myflashjob/views.py
@@ -1,6 +1,73 @@
 from django.http import JsonResponse
+import json
 from .models import Personne, Entreprise, Annonce, Candidature
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import check_password
+from django.shortcuts import redirect
 
+@csrf_exempt
+def login(request):
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+            email = data.get("email")
+            password = data.get("password")
+            type_user = data.get("type")  # "personne" ou "entreprise"
+
+            if type_user == "personne":
+                from .models import Personne
+                user = Personne.objects.get(email=email)
+            elif type_user == "entreprise":
+                from .models import Entreprise
+                user = Entreprise.objects.get(email=email)
+            else:
+                return JsonResponse({"success": False, "error": "Type d'utilisateur invalide"})
+
+            if check_password(password, user.mot_de_passe):
+                return JsonResponse({"success": True, "id": user.id, "type": type_user})
+            else:
+                return JsonResponse({"success": False, "error": "Mot de passe incorrect"})
+        except (Personne.DoesNotExist, Entreprise.DoesNotExist):
+            return JsonResponse({"success": False, "error": "Utilisateur non trouvé"})
+        except Exception as e:
+            return JsonResponse({"success": False, "error": str(e)})
+    return JsonResponse({"success": False, "error": "Méthode non autorisée"})
+
+
+
+@csrf_exempt
+def create_entreprise(request):
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+            entreprise = Entreprise.objects.create(
+                nom=data["nom"],
+                email=data["email"],
+                departement=data.get("departement", ""),
+                mot_de_passe=make_password(data["mot_de_passe"])
+            )
+            return JsonResponse({"message": "Entreprise créée avec succès", "id": entreprise.id})
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=400)
+    return JsonResponse({"error": "Méthode non autorisée"}, status=405)
+
+@csrf_exempt
+def create_personnes(request):
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+            personne = Personne.objects.create(
+                nom=data["nom"],
+                email=data["email"],
+                mot_de_passe=make_password(data["mot_de_passe"])
+            )
+            return JsonResponse({"message": "Compte Utilisateur créée avec succès", "id": personne.id})
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=400)
+    return JsonResponse({"error": "Méthode non autorisée"}, status=405)
+
+    
 # Liste des Personnes
 def personnes_list(request):
     personnes = Personne.objects.all().values(
@@ -80,4 +147,34 @@ def annonces_details_list(request, id):
         raise Http404("Annonce non trouvée")
     
     return JsonResponse(annonce)
+
+def personnes_details_list(request, id):
+    try:
+        personne = Personne.objects.values(
+        'id',
+        'nom',
+        'email',
+        'departement',
+        'created_at',
+        'updated_at'
+        ).get(id=id)
+    except Personne.DoesNotExist:
+        raise Http404("Personnes non trouvée")
+    
+    return JsonResponse(personne)
+
+def entreprises_details_list(request, id):
+    try:
+        entreprise = Entreprise.objects.values(
+        'id',
+        'nom',
+        'email',
+        'departement',
+        'created_at',
+        'updated_at'
+        ).get(id=id)
+    except Entreprise.DoesNotExist:
+        raise Http404("Entreprise non trouvée")
+    
+    return JsonResponse(entreprise)
 
